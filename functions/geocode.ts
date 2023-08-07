8f75e600f09aed6d6b6407b190e0411c27e301b2
Barnaby <22575741+barnabwhy@users.noreply.github.com>
2023-08-07 20:48:10 +0100

    remove google api dep and support no geoloc

--- functions/geocode.ts
+++
@@ -1,6 +0,0 @@
-const GOOGLE_API_KEY = "AIzaSyCe4v19GE_1Y8bN-x31b3__RxRXvNWDZPk";
-export async function onRequest(context) {
-    let lat = new URL(context.request.url).searchParams.get("lat");
-    let lon = new URL(context.request.url).searchParams.get("lon");
-    return fetch("https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lon+"&sensor=true&key="+GOOGLE_API_KEY);
-}
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // const IKEA_API_STORES = "https://www.ikea.com/gb/en/meta-data/navigation/stores-detailed.json";
 // const IKEA_API_STORES = "stores-detailed.json";
 const IKEA_API_STORES = "stores";
-const IKEA_API_STOCK = "https://api.ingka.ikea.com/cia/availabilities/ru/gb?itemNos=%itemNos%&zip=%postCode%";
+const IKEA_API_STOCK = "https://api.ingka.ikea.com/cia/availabilities/ru/gb?itemNos=%itemNos%";
 const IKEA_CLIENT_ID = "b6c117e5-ae61-4ef5-b4cc-e0b1e37f0631";
 
 let storeData = [];
@@ -9,19 +9,19 @@ let storeData = [];
     fetch(IKEA_API_STORES)
         .then((response) => response.json())
         .then(async (data) => {
-            let position = await getCurrentPosition();
-            storeData = data.sort((a, b) => getDistance(position.coords.latitude, position.coords.longitude, a.lat, a.lng)
-                - getDistance(position.coords.latitude, position.coords.longitude, b.lat, b.lng));
+            let { success, position } = await getCurrentPosition();
+            storeData = success ? data.sort((a, b) => getDistance(position.coords.latitude, position.coords.longitude, a.lat, a.lng)
+                - getDistance(position.coords.latitude, position.coords.longitude, b.lat, b.lng)) : data;
 
-            renderBlahajStock(await getBlahajStockNearby());
+            renderBlahajStock(await getBlahajStock());
         })
         .catch(err => {
             document.querySelector("#blahaj").innerHTML = `<h3>${err}</h3>`;
         });
 })();
 
-async function getBlahajStockNearby() {
-    let stock = await getIkeaStock([20540663, 30373588], await getCurrentPostCode());
+async function getBlahajStock() {
+    let stock = await getIkeaStock([20540663, 30373588], null);
     let storeStock = {};
     for(let s of stock) {
         // console.log(s)
@@ -39,55 +39,57 @@ async function getBlahajStockNearby() {
 }
 
 async function renderBlahajStock(storeStock) {
-    let position = await getCurrentPosition();
+    let { success, position } = await getCurrentPosition();
     let blahajEl = document.querySelector("#blahaj");
     blahajEl.innerHTML = "";
-    for(let i = 0; i < storeData.length; i++) {
-        let store = storeData[i];
-        let avail = storeStock[store.id].availableStocks.find(s => s.itemNo == 20540663);
-        if(avail && avail.quantity > 0) {
-            blahajEl.innerHTML += `<div>
-            <h3>Nearest BLÅHAJ (55cm)</h3>
-            <p>${store.name} (${getDistance(position.coords.latitude, position.coords.longitude, store.lat, store.lng).toFixed(1)}km)</p>
-            <p>${avail.quantity} in stock</p>
-            </div>`
-            break;
+    if(success) {
+        for(let i = 0; i < storeData.length; i++) {
+            let store = storeData[i];
+            let avail = storeStock[store.id].availableStocks.find(s => s.itemNo == 20540663);
+            if(avail && avail.quantity > 0) {
+                blahajEl.innerHTML += `<div>
+                <h3>Nearest BLÅHAJ (55cm)</h3>
+                <p>${store.name} (${getDistance(position.coords.latitude, position.coords.longitude, store.lat, store.lng).toFixed(1)}km)</p>
+                <p>${avail.quantity} in stock</p>
+                </div>`
+                break;
+            }
+            if(i == storeData.length - 1) {
+                blahajEl.innerHTML += `<div>
+                <h3>Nearest BLÅHAJ (55cm)</h3>
+                <p>Out of stock everywhere</p>
+                <p>😭</p>
+                </div>`
+            }
         }
-        if(i == storeData.length - 1) {
-            blahajEl.innerHTML += `<div>
-            <h3>Nearest BLÅHAJ (55cm)</h3>
-            <p>Out of stock everywhere</p>
-            <p>😭</p>
-            </div>`
-        }
-    }
-    for(let i = 0; i < storeData.length; i++) {
-        let store = storeData[i];
-        let avail = storeStock[store.id].availableStocks.find(s => s.itemNo == 30373588) || { quantity: 0, restocks: [] };
-        if(avail && avail.quantity > 0) {
-            blahajEl.innerHTML += `<div>
-            <h3>Nearest BLÅHAJ (100cm)</h3>
-            <p>${store.name} (${getDistance(position.coords.latitude, position.coords.longitude, store.lat, store.lng).toFixed(1)}km)</p>
-            <p>${avail.quantity} in stock</p>
-            </div>`
-            break;
-        }
-        if(i == storeData.length - 1) {
-            blahajEl.innerHTML += `<div>
-            <h3>Nearest BLÅHAJ (100cm)</h3>
-            <p>Out of stock everywhere</p>
-            <p>😭</p>
-            </div>`
+        for(let i = 0; i < storeData.length; i++) {
+            let store = storeData[i];
+            let avail = storeStock[store.id].availableStocks.find(s => s.itemNo == 30373588) || { quantity: 0, restocks: [] };
+            if(avail && avail.quantity > 0) {
+                blahajEl.innerHTML += `<div>
+                <h3>Nearest BLÅHAJ (100cm)</h3>
+                <p>${store.name} (${getDistance(position.coords.latitude, position.coords.longitude, store.lat, store.lng).toFixed(1)}km)</p>
+                <p>${avail.quantity} in stock</p>
+                </div>`
+                break;
+            }
+            if(i == storeData.length - 1) {
+                blahajEl.innerHTML += `<div>
+                <h3>Nearest BLÅHAJ (100cm)</h3>
+                <p>Out of stock everywhere</p>
+                <p>😭</p>
+                </div>`
+            }
         }
     }
 
-    blahajEl.innerHTML += '<br><h2>IKEA stores (sorted by distance)</h2>';
+    blahajEl.innerHTML += `<br><h2>IKEA stores${success ? ' (sorted by distance)' : ''}</h2>`;
 
     for(let store of storeData) {
         let avail55 = storeStock[store.id].availableStocks.find(s => s.itemNo == 20540663) || { quantity: 0, restocks: [] };
         let avail100 = storeStock[store.id].availableStocks.find(s => s.itemNo == 30373588) || { quantity: 0, restocks: [] };
         blahajEl.innerHTML += `<div>
-        <h3>${store.name} (${getDistance(position.coords.latitude, position.coords.longitude, store.lat, store.lng).toFixed(1)}km)</h3>
+        <h3>${store.name}${success ? `(${getDistance(position.coords.latitude, position.coords.longitude, store.lat, store.lng).toFixed(1)}km)` : ''}</h3>
         <span>BLÅHAJ (55cm): ${avail55.quantity} in stock</span> ${(avail55.restocks.length > 0 ? `<h6>Restocking ${avail55.restocks[0].quantity} between ${avail55.restocks[0].earliestDate} and ${avail55.restocks[0].latestDate}</h6>` : "")}
         <br>
         <span>BLÅHAJ (100cm): ${avail100.quantity} in stock</span> ${(avail100.restocks.length > 0 ? `<h6>Restocking ${avail100.restocks[0].quantity} between ${avail100.restocks[0].earliestDate} and ${avail100.restocks[0].latestDate}<h6>` : "")}
@@ -95,46 +97,9 @@ async function renderBlahajStock(storeStock) {
     }
 }
 
-let cachedPostCode;
-function getCurrentPostCode() {
-    return new Promise(async (resolve, reject) => {
-        if(cachedPostCode)
-            return resolve(cachedPostCode);
-
-        let position = await getCurrentPosition();
-        reverseGeoLookup(position.coords.latitude, position.coords.longitude)
-        .then(postCode => {
-            cachedPostCode = postCode;
-            resolve(postCode);
-        })
-        .catch(resolve(null));
-    })
-}
-
-function reverseGeoLookup(lat, lon) {
+function getIkeaStock(itemNos) {
     return new Promise((resolve, reject) => {
-        fetch("geocode?lat="+lat+"&lon="+lon)
-            .then((response) => response.json())
-            .then((data) => {
-                let result = data.results;
-                for(var i = 0, length = result.length; i < length; i++) {
-                    //each result has an address with multiple parts (it's all in the reference)
-                    for(var j = 0; j < result[i].address_components.length; j++) {
-                        var component = result[i].address_components[j]
-                        //if the address component has postal code then write it out
-                        if(~component.types.indexOf("postal_code")) {
-                            return resolve(component.long_name)
-                        }
-                    }
-                }
-                reject("No postcode found");
-            });
-    });
-}
-
-function getIkeaStock(itemNos, postCode) {
-    return new Promise((resolve, reject) => {
-        fetch(IKEA_API_STOCK.replace("%itemNos%", itemNos.join(",")).replace("%postCode%", postCode),
+        fetch(IKEA_API_STOCK.replace("%itemNos%", itemNos.join(",")),
             {
                 headers: {
                     'X-Client-ID': IKEA_CLIENT_ID
@@ -169,22 +134,22 @@ function deg2Rad(Value)
 
 let cachedPosition;
 function getCurrentPosition() {
-    return new Promise((resolve, reject) => {
+    return new Promise(resolve => {
         if(cachedPosition)
-            return resolve(cachedPosition);
+            return resolve({ success: true, position: cachedPosition });
 
         if (navigator.geolocation) {
             var timeoutVal = 10 * 1000 * 1000;
             navigator.geolocation.getCurrentPosition(
                 (position) => {
                     cachedPosition = position;
-                    resolve(position);
+                    resolve({ success: true, position });
                 },
-                (err) => reject(err.message),
+                (err) => resolve({ success: false }),
                 { enableHighAccuracy: true, timeout: timeoutVal, maximumAge: 0 }
             );
         } else {
-            reject("Geolocation not supported");
+            resolve({ success: false });
         }
     });
 }
